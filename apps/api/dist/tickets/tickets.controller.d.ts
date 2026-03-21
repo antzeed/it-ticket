4702b05441e9fbae5a1e91aa014784bd40f65532
Kittapat Chanapai <macos_kbao@kittapat.cha>
2026-03-21 20:37:06 +0700

    fix(api): enable cors for frontend client access

--- apps/api/dist/tickets/tickets.controller.d.ts
+++ apps/api/dist/tickets/tickets.controller.d.ts
@@ -5,29 +5,29 @@ export declare class TicketsController {
     constructor(ticketsService: TicketsService);
     create(req: any, createTicketDto: CreateTicketDto): Promise<{
         id: string;
+        createdAt: Date;
+        updatedAt: Date;
         title: string;
         description: string;
         status: import("@prisma/client").$Enums.TicketStatus;
-        createdAt: Date;
-        updatedAt: Date;
         authorId: string;
     }>;
     findAll(req: any): Promise<{
         id: string;
+        createdAt: Date;
+        updatedAt: Date;
         title: string;
         description: string;
         status: import("@prisma/client").$Enums.TicketStatus;
-        createdAt: Date;
-        updatedAt: Date;
         authorId: string;
     }[]>;
     findOne(id: string, req: any): Promise<{
         id: string;
+        createdAt: Date;
+        updatedAt: Date;
         title: string;
         description: string;
         status: import("@prisma/client").$Enums.TicketStatus;
-        createdAt: Date;
-        updatedAt: Date;
         authorId: string;
     }>;
 }
--- apps/api/dist/tickets/tickets.service.d.ts
+++ apps/api/dist/tickets/tickets.service.d.ts
@@ -5,29 +5,29 @@ export declare class TicketsService {
     constructor(prisma: PrismaService);
     create(userId: string, createTicketDto: CreateTicketDto): Promise<{
         id: string;
+        createdAt: Date;
+        updatedAt: Date;
         title: string;
         description: string;
         status: import("@prisma/client").$Enums.TicketStatus;
-        createdAt: Date;
-        updatedAt: Date;
         authorId: string;
     }>;
     findAll(user: any): Promise<{
         id: string;
+        createdAt: Date;
+        updatedAt: Date;
         title: string;
         description: string;
         status: import("@prisma/client").$Enums.TicketStatus;
-        createdAt: Date;
-        updatedAt: Date;
         authorId: string;
     }[]>;
     findOne(id: string, user: any): Promise<{
         id: string;
+        createdAt: Date;
+        updatedAt: Date;
         title: string;
         description: string;
         status: import("@prisma/client").$Enums.TicketStatus;
-        createdAt: Date;
-        updatedAt: Date;
         authorId: string;
     }>;
 }
